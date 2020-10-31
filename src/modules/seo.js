0b20ff7e9b1d86d81108ea12fb3983e5c9d7c067
Pouriya Babaali <pouriya.babaali1998@gmail.com>
2020-10-31 13:16:16 +0330

    ..

--- src/modules/seo.js
+++ src/modules/seo.js
@@ -1,13 +1,6 @@
-import { SITE_URL, RC_ENV } from '../../env';
+import { SITE_URL } from '../../env';
 
 const Hashids = require('hashids/cjs');
-
-export const footerscripts = () => {
-  if (process.env.NODE_ENV === 'production' && !RC_ENV) {
-    return "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-PKPHZ2P');</script> <noscript><iframe src='https://www.googletagmanager.com/ns.html?id=GTM-PKPHZ2P' height='0' width='0' style='display:none;visibility:hidden'></iframe></noscript><script>";
-  }
-  return null;
-};
 
 export const createNewLink = (to) => {
   const res = to
