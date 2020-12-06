a973b3b72499b3b549d4dbd0da368ef8b1ce22f6
pouriyababaali1998 <pouriya.babaali1998@gmail.com>
2020-12-06 11:41:24 +0330

    change title for best .....

--- src/pages/tags/[slug].js
+++ src/pages/tags/[slug].js
@@ -5,19 +5,26 @@ import {
   getLastestPodcast,
   getTopBestPodcast,
 } from '@/api/music';
+import { NextSeo } from 'next-seo';
 import Layout from '@/components/Layout/Layout';
 import MusicTable from '@/components/MusicTable/MusicTable';
 import { Col, Divider, Row } from 'antd';
-const index = ({ musics, subject, title }) => {
+const index = ({ musics, subject, title, seoTitle, seoDescripton }) => {
   return (
-    <Layout>
-      <Row className="pt-5">
-        <Divider orientation="right">{title}</Divider>
-        <Col xs={24}>
-          <MusicTable data={musics?.musics} subjects={`${subject}-in-table`} />
-        </Col>
-      </Row>
-    </Layout>
+    <>
+      <NextSeo title={seoTitle} description={seoDescripton} />;
+      <Layout>
+        <Row className="pt-5">
+          <Divider orientation="right">{title}</Divider>
+          <Col xs={24}>
+            <MusicTable
+              data={musics?.musics}
+              subjects={`${subject}-in-table`}
+            />
+          </Col>
+        </Row>
+      </Layout>
+    </>
   );
 };
 
@@ -25,12 +32,18 @@ export async function getServerSideProps({ query }) {
   let musics;
   let subject = null;
   let title = null;
+  let seoTitle = null;
+  let seoDescripton = null;
   switch (query.slug) {
     case 'top-best-music':
       try {
         musics = await getBestMusic(50);
         subject = query.slug;
         title = 'بهترین های کاست';
+        seoTitle =
+          'دانلود بهترین آهنگ های جدید ایرانی 1399 (با آخرین کیفیت و لینک مستقیم )';
+        seoDescripton =
+          'رادیوکاست ، پخَش آنلاین و دانلود بهترین آهنگ های ایرانی با آخرین کیفیت و لینک مستقیم ، دانلود همه ی آهنگ های ایرانی جدید';
       } catch (e) {
         console.log(e);
       }
@@ -40,6 +53,10 @@ export async function getServerSideProps({ query }) {
         musics = await getLastMusic(50);
         subject = query.slug;
         title = ' جدیدترین کاست ها';
+        seoTitle =
+          'دانلود جدیدترین و بروزترین آهنگ های ایرانی 1399 (با آخرین کیفیت و لینک مستقیم)';
+        seoDescripton =
+          'رادیوکاست ، پخَش آنلاین و دانلود بهترین آهنگ های ایرانی با آخرین کیفیت و لینک مستقیم ، دانلود همه ی آهنگ های ایرانی جدید';
       } catch (e) {
         console.log(e);
       }
@@ -49,6 +66,10 @@ export async function getServerSideProps({ query }) {
         musics = await getLastMusicInSite(50);
         subject = query.slug;
         title = ' آخرین کاست های اضافه شده';
+        seoTitle =
+          'دانلود جدیدترین آهنگ های ایرانی 1399 (با آخرین کیفیت و لینک مستقیم)';
+        seoDescripton =
+          'رادیوکاست ، پخَش آنلاین و دانلود بهترین آهنگ های ایرانی با آخرین کیفیت و لینک مستقیم ، دانلود همه ی آهنگ های ایرانی جدید';
       } catch (e) {
         console.log(e);
       }
@@ -58,6 +79,10 @@ export async function getServerSideProps({ query }) {
         musics = await getLastestPodcast(50);
         subject = query.slug;
         title = 'آخرین پادکست ها';
+        seoTitle =
+          'دانلود جدیدترین پادکست های عاشقانه ، تکنولوژی ، اخبار ، ورزشی ، داستان ،با آخرین کیفیت و لینک مستقیم';
+        seoDescripton =
+          'رادیوکاست ، پخَش آنلاین و دانلود بهترین پادکست های ایرانی با آخرین کیفیت و لینک مستقیم ، دانلود همه ی آهنگ های ایرانی جدید';
       } catch (e) {
         console.log(e);
       }
@@ -68,6 +93,10 @@ export async function getServerSideProps({ query }) {
         musics = await getTopBestPodcast(50);
         subject = query.slug;
         title = 'بهترین پادکست ها';
+        seoTitle =
+          'دانلود جدیدترین پادکست های عاشقانه ، تکنولوژی ، اخبار ، ورزشی ، داستان ،با آخرین کیفیت و لینک مستقیم';
+        seoDescripton =
+          'رادیوکاست ، پخَش آنلاین و دانلود بهترین پادکست های ایرانی با آخرین کیفیت و لینک مستقیم ، دانلود همه ی آهنگ های ایرانی جدید';
       } catch (e) {
         console.log(e);
       }
@@ -85,6 +114,8 @@ export async function getServerSideProps({ query }) {
       musics: musics?.data?.data || null,
       subject,
       title,
+      seoTitle,
+      seoDescripton,
     },
   };
 }
